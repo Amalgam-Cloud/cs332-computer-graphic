b6f139387c36cdfea8f94b6ee91032a517d3db3c
Georgrigz <ggrigoryan@sfedu.ru>
2021-09-23 17:45:55 +0300

    hist

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -32,9 +32,15 @@ namespace cs332ComputerGraphic
             this.REDpictureBox = new System.Windows.Forms.PictureBox();
             this.BLUEpictureBox = new System.Windows.Forms.PictureBox();
             this.GREENpictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.REDpictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.BLUEpictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.GREENpictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // REDpictureBox
@@ -61,11 +67,38 @@ namespace cs332ComputerGraphic
             this.GREENpictureBox.TabIndex = 2;
             this.GREENpictureBox.TabStop = false;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(390, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(372, 285);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(390, 303);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(372, 285);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(390, 594);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(372, 285);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 885);
+            this.ClientSize = new System.Drawing.Size(770, 885);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.GREENpictureBox);
             this.Controls.Add(this.BLUEpictureBox);
             this.Controls.Add(this.REDpictureBox);
@@ -75,6 +108,9 @@ namespace cs332ComputerGraphic
             ((System.ComponentModel.ISupportInitialize)(this.REDpictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.BLUEpictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.GREENpictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -84,5 +120,8 @@ namespace cs332ComputerGraphic
         private System.Windows.Forms.PictureBox REDpictureBox;
         private System.Windows.Forms.PictureBox BLUEpictureBox;
         private System.Windows.Forms.PictureBox GREENpictureBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -31,16 +31,81 @@ namespace cs332ComputerGraphic
             return result;
         }
 
+        public static Bitmap RGBHist(Bitmap res)
+        {
+            int Width = 256, Height = res.Height;
+
+            Bitmap Bars = new Bitmap(Width,Height);
+
+            int[] R = new int[256];
+            int[] G = new int[256];
+            int[] B = new int[256];
+
+            int i, j;
+            Color color;
+            for (i = 0; i < res.Width; i++)
+                for (j = 0; j < res.Height-1; ++j)
+                {
+                    color = res.GetPixel(i, j);
+                    R[color.R]++;
+                    G[color.G]++;
+                    B[color.B]++;
+                }
+
+            int max = 0;
+
+            for (i = 0; i < 256; ++i)
+            {
+                if (R[i] > max)
+                    max = R[i];
+                if (G[i] > max)
+                    max = G[i];
+                if (B[i] > max)
+                    max = B[i];
+            }
+
+            double point = (double)max / Height;
+
+            for (i = 0; i < Width - 3; ++i)
+            {
+                for (j = Height - 1; j > Height - R[i / 3] / point; --j)
+                {
+                    Bars.SetPixel(i, j, Color.Red);
+                }
+                ++i;
+                for (j = Height - 1; j > Height - G[i / 3] / point; --j)
+                {
+                    Bars.SetPixel(i, j, Color.Green);
+                }
+                ++i;
+                for (j = Height - 1; j > Height - B[i / 3] / point; --j)
+                {
+                    Bars.SetPixel(i, j, Color.Blue);
+                }
+            }
+            return Bars;
+        } 
+
+
         public Form2(Bitmap res)
         {
-            InitializeComponent();
+            InitializeComponent(); 
             Bitmap[] RGB = ConvertToRGB(res);
             REDpictureBox.Image = RGB[0];
             REDpictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
-            GREENpictureBox.Image = RGB[1];
+            GREENpictureBox.Image = RGB[2];
             GREENpictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
-            BLUEpictureBox.Image = RGB[2];
+            BLUEpictureBox.Image = RGB[1];
             BLUEpictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            Bitmap Hist = RGBHist(RGB[0]);
+            pictureBox1.Image = Hist;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            Hist = RGBHist(RGB[1]);
+            pictureBox2.Image = Hist;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            Hist = RGBHist(RGB[2]);
+            pictureBox3.Image = Hist;
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
         }
 
        
