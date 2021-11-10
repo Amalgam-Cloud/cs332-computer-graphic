490abd14371cf19b3a74a313fb5b3799c407766f
Georgrigz <52125456+Georgrigz@users.noreply.github.com>
2021-11-10 17:13:18 +0300

    UDP

---
+++ Assignment8/Graph3D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffineTransformationsIn3D
+{
+    class Graph3D
+    {
+    }
+}
